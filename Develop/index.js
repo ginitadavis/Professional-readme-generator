0c8e2faee359681f260f4504b402b6b8dd859190
ginitadavis <120149064+ginitadavis@users.noreply.github.com>
2023-02-28 22:16:11 -0500

    Run the first test of my application after finishing the requirements

--- Develop/index.js
+++ Develop/index.js
@@ -1,46 +1,128 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer')
 // TODO: Create an array of questions for user input
-const questions = ['What was your motivation?', 'Why did you build this project?',
-'What problem does it solve?', 'What did you learn?'];
+const questions = ['What is the title of your project','Provide a description of your project', 'Provide a detailed installation process', 'Explain the usage of the application',
+'What is your github username?', 'Test instructions'];
+const license = ['Apache License v2.0','GNU General Public License v3.0','MIT License'];
+// const apache = `Copyright [yyyy] [name of copyright owner]
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.`
+
+// const GNU = `GNU General Public License v3.0
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// See <https://www.gnu.org/licenses/>.`
+
+// const MIT = `[MIT](https://choosealicense.com/licenses/mit/)`
+const fs = require('fs');
 let response = [];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    // use the fs wrte file function 
+    // use the fs write file function 
     // pass the filename and the data to the function
+
+    const completedReadme = `
+# ${data.proTitle}
+
+## Description
+
+${data.proDescription}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+
+## Installation
+${data.proInstallation}
+
+## Usage
+
+${data.proUsage}
+
+![alt text](assets/images/inquirer-screenshot.png)
+
+## Contributing
+- [@ginitadavis](${data.github})
+
+## Tests
+${data.testInstructions}
+
+## Questions
+Link to my github profile ${data.github}
+
+## License
+${data.license}
+
+    `
+    fs.writeFile(fileName, completedReadme, function(){
+        console.log('README successfully created!');
+    })
 }
 
 // TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
-
-inquirer
+function init() {
+    inquirer
     .prompt([
         {
             type: 'input',
             message: questions[0],
-            name: response[0]
+            name: 'proTitle'
         },
         {
             type: 'input',
             message: questions[1],
-            name: response[1]
+            name: 'proDescription'
         },
         {
             type: 'input',
             message: questions[2],
-            name: response[2]
+            name: 'proInstallation'
         },
         {
             type: 'input',
             message: questions[3],
-            name: response[3]
+            name: 'proUsage'
         },
+        {
+            type: 'input',
+            message: questions[4],
+            name: 'github'
+        },
+        {
+            type: 'input',
+            message: questions[5],
+            name: 'testInstructions'
+        },
+        {
+            type: 'checkbox',
+            message: 'Please select a license',
+            name: 'license',
+            choices: license,
+        }
     ])
     .then ((data) => {
-        const fileName = 'README.md';
-        
+        const fileName = '../README.md';
+        writeToFile(fileName, data);
     })
+}
+
+// Function call to initialize app
+init();
+
--- README.md
+++ README.md
@@ -1,48 +1,37 @@
-# ${PROJECT TITLE}
+
+# Professional Readme
 
 ## Description
 
 This application was built to design a Readme file. I wanted to apply my new knowlegde of node.js to create a server application using the ES6 syntax in javascript and the terminal.
-After this project I feel more confident using node.js
 
 ## Table of Contents
 
 - [Installation](#installation)
 - [Usage](#usage)
-- [Credits](#credits)
+- [Contributing](#contributing)
 - [Tests](#tests)
 - [License](#license)
 
 ## Installation
-To start running this project in a computer, the user needs to install node.js in their computer. In the browser search for node version no lower than 16.19.1 and download it to the computer.
-Check if node.js was installed going to gitbash and typing "node", the terminal will show the version of node installed.
-```bash
-node
-Welcome to Node.js v16.19.1.
-```
-
-Now open VS Code and in the terminal type npm inquirer@8.2.4 to install the inquirer package that assists with the prompt questions that designs the readme file. 
-Now the computer is ready to read the code.
+After this project I feel more confident using node.jsTo start running this project in a computer, the user needs to install node.js in their computer. In the browser search for a node version no lower than 16.19.1 and download it to the computer.
 
 ## Usage
 
-The video shows an user going to the terminal and typing node.js and then a series of questions pop up. When the user answers the questions, a readme file is generated and those responses are added to the file.
+Check if node.js was installed going to gitbash and typing "node", the terminal will show the version of node installed.The video shows an user going to the terminal and typing node.js and then a series of questions pop up. When the user answers the questions, a readme file is generated and those responses are added to the file.
 
 ![alt text](assets/images/inquirer-screenshot.png)
 
-## Credits
-- ${[@ginitadavis](https://github.com/ginitadavis/)}
+## Contributing
+- [@ginitadavis](ginitadavis)
 
 ## Tests
+This is the first test of my application
 
+## Questions
+Link to my github profile ginitadavis
 
 ## License
-MIT License
+Apache License v2.0,GNU General Public License v3.0,MIT License
 
-Copyright (c) [2022] [Gina Davis]
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+    
